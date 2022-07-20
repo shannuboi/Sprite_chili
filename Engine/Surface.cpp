e79bb07e514c5a7b03ee1a478c53b698c0acd5b5
shannu boi <shayan.khan.xyz@gmail.com>
2022-07-20 21:52:49 +0500

    Added distructor, ro3, and some assertions

--- Engine/Surface.cpp
+++ Engine/Surface.cpp
@@ -1,4 +1,5 @@
 #include "Surface.h"
+#include <assert.h>
 
 Surface::Surface(int width, int height)
 	:
@@ -8,13 +9,62 @@ Surface::Surface(int width, int height)
 {
 }
 
+Surface::Surface(const Surface & src)
+	:
+	width(src.width),
+	height(src.height),
+	pPixels(new Color[width * height])
+{
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			pPixels[y * width + x] = src.pPixels[y * width + x];
+		}
+	}
+}
+
+Surface::~Surface()
+{
+	delete[] pPixels;
+	pPixels = nullptr;
+}
+
+Surface & Surface::operator=(const Surface & rhs)
+{
+	delete[] pPixels;
+	pPixels = nullptr;
+
+	width = rhs.width;
+	height = rhs.height;
+	pPixels = new Color[width * height];
+
+	for (int x = 0; x < width; x++)
+	{
+		for (int y = 0; y < height; y++)
+		{
+			pPixels[y * width + x] = rhs.pPixels[y * width + x];
+		}
+	}
+
+	return *this;
+}
+
 const Color & Surface::GetColor(int x, int y) const
 {
+	assert(x >= 0);
+	assert(x < width);
+	assert(y >= 0);
+	assert(y < height);
 	return pPixels[y * width + x];
 }
 
 void Surface::SetColor(int x, int y, const Color & c)
 {
+	assert(x >= 0);
+	assert(x < width);
+	assert(y >= 0);
+	assert(y < height);
 	pPixels[y * width + x] = c;
 }
 
--- Engine/Surface.h
+++ Engine/Surface.h
@@ -1,16 +1,20 @@
 #pragma once
 
 #include "Colors.h"
+#include <string>
 
 class Surface
 {
 public:
 	Surface(int width, int height);
+	Surface(const Surface& src);
+	~Surface();
+	Surface& operator=(const Surface& rhs);
 	const Color& GetColor(int x, int y) const;
 	void SetColor(int x, int y, const Color& c);
 	int GetWidth() const;
 	int GetHeight() const;
 private:
-	const int width, height;
+	int width, height;
 	Color* pPixels = nullptr;
 };
